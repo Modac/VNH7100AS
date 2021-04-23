8faeaa9e95792449a1b4e50bfcafb857901f1d4a
Modac <Modac@users.noreply.github.com>
2021-04-23 11:45:09 +0200

    Update to fit VNH7100AS

--- src/VNH7100AS.h
+++ src/VNH7100AS.h
@@ -1,6 +1,6 @@
 
 /*
- * VNH3SP30 Arduino motor driver library
+ * VNH7100AS Arduino motor driver library
  * 
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public 
@@ -8,17 +8,18 @@
  * version 2.1 of the License, or (at your option) any later version.
  *   
  * Created 2 June 2019 Bart Mellink
+ * Modified 23 April 2021 Pascal Goldbrunner
  */
-#ifndef VNH3SP30_h
-#define VNH3SP30_h
+#ifndef VNH7100AS_h
+#define VNH7100AS_h
 
 #include <inttypes.h>
 
 
-class VNH3SP30 {
+class VNH7100AS {
 
 public:
-  void begin(int8_t pwmPin=-1, int8_t inaPin=-1, int8_t inbPin=-1, int8_t diagPin=-1, int8_t csPin=-1);
+  void begin(int8_t pwmPin=-1, int8_t inaPin=-1, int8_t inbPin=-1, int8_t sel0Pin=-1, int8_t csPin=-1);
   uint8_t setSpeed(int speed); 
   uint8_t brake(int brakePower);
   uint8_t status(); 
@@ -29,7 +30,7 @@ private:
   uint8_t _pwmPin = -1;
   uint8_t _inaPin = -1;
   uint8_t _inbPin = -1;
-  uint8_t _diagPin = -1;
+  uint8_t _sel0Pin = -1;
   uint8_t _csPin = -1;
   uint8_t forward = true; // last speed command is forward
 };
