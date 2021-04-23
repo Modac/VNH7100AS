9d3b1a14a33f025355b1a310c5b88ba35295359a
Modac <Modac@users.noreply.github.com>
2021-04-23 12:06:55 +0200

    Update and rename VNH3SP30.cpp to VNH7100AS.cpp

--- src/VNH7100AS.cpp
+++ src/VNH7100AS.cpp
@@ -10,27 +10,32 @@
  */
 
 #include <Arduino.h>
-#include <VNH3SP30.h>
+#include <VNH7100AS.h>
 
 #ifdef ARDUINO_ARCH_ESP32
   #include <analogWrite.h>
 #endif
 
-void VNH3SP30::begin(int8_t pwmPin, int8_t inaPin, int8_t inbPin, int8_t diagPin, int8_t csPin) {
+void VNH7100AS::begin(int8_t pwmPin, int8_t inaPin, int8_t inbPin, int8_t sel0Pin, int8_t csPin) {
   this->_pwmPin = pwmPin;
   this->_inaPin = inaPin;
   this->_inbPin = inbPin;
-  this->_diagPin = diagPin;
+  this->_sel0Pin = sel0Pin;
   this->_csPin = csPin;
   if (pwmPin>0) pinMode(pwmPin, OUTPUT);
   if (inaPin>0) pinMode(inaPin, OUTPUT);
   if (inbPin>0) pinMode(inbPin, OUTPUT);
-  if (diagPin>0) pinMode(diagPin, INPUT_PULLUP);
-  if (csPin>0) pinMode(csPin, INPUT); // analog input
+  if (sel0Pin>0 && csPin>0) {
+    pinMode(diagPin, INPUT_PULLUP);
+    pinMode(csPin, INPUT); // analog input
+  } else {
+    this->_sel0Pin = -1;
+    this->_csPin = -1;
+  }
   this->setSpeed(0);
 }
 
-uint8_t VNH3SP30::setSpeed(int speed) {
+uint8_t VNH7100AS::setSpeed(int speed) {
   // Ensure we do not reverse the ina and inb setting in case speed==0 to guarantee the motor
   // will free run to a stop (if you reverse ina and inb setting the controller will issue a full brake)
   if (speed>0 || (speed==0 && this->forward)) {
@@ -45,26 +50,30 @@ uint8_t VNH3SP30::setSpeed(int speed) {
   }
   if (speed>400) speed = 400;
   this->speed = (this->forward ? speed : -speed);
+  delayMicroseconds(20);			// to wake from standby wait 20us after setting INA/INB for PWM. Just do it always
   analogWrite(this->_pwmPin, speed * 51 / 80); // map 400 to 255 and generate pwm
   return this->status();
 }
 
-uint8_t VNH3SP30::brake(int brakePower) {
+uint8_t VNH7100AS::brake(int brakePower) {
   if (brakePower<0) brakePower = 0;
   if (brakePower>400) brakePower = 400;
   digitalWrite(this->_inaPin, LOW);
   digitalWrite(this->_inbPin, LOW);
+  delayMicroseconds(20);			// to wake from standby wait 20us after setting INA/INB for PWM. Just do it always
   analogWrite(this->_pwmPin, brakePower * 51 / 80); // map 400 to 255
   this->speed = 0;
   return this->status();
 }
 
-uint8_t VNH3SP30::status() {
-	if (this->_diagPin<=0) return true;
-	return !digitalRead(this->_diagPin);
+uint8_t VNH7100AS::isFault() {
+	if (this->_sel0Pin<=0) return false;
+	// TODO
+	//return !digitalRead(this->_diagPin);
 } 
 
-int VNH3SP30::motorCurrent() {
-	if (this->_csPin<0) return 0;
-	return analogRead(this->_csPin);
+int VNH7100AS::motorCurrent() {
+	// TODO
+	//if (this->_csPin<0) return 0;
+	//return analogRead(this->_csPin);
 }
